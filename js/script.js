448556121cc3e1d3e1fea65d45daa470d10487c7
Gus2323 <gustavocastaneda2323@gmail.com>
2019-11-29 19:39:08 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
 var songs = [
     "La La Land",
-    "Myself",
+    "Creep On Me",
     "Up",
     "Candy Paint"
 ];
 
 var artists = [
-    "NAV",
-    "NAV",
-    "NAV",
-    "Post Malone"
+    " Bryce Vine",
+    " GASHI",
+    " NAV",
+    " Post Malone"
 ];
 
 var images = [
     "https://upload.wikimedia.org/wikipedia/en/thumb/b/b1/Bryce_Vine_-_La_La_Land.png/220px-Bryce_Vine_-_La_La_Land.png",
-    "https://i.ytimg.com/vi/f9gfqGoPGaU/maxresdefault.jpg",
+    "https://cdn.smehost.net/rcarecordscom-usrcaprod/wp-content/uploads/2018/10/hyTdVcv-561x561.jpg",
     "https://images.genius.com/02794f15f7d5ae41cd9b27aba4803bb8.500x500x1.jpg",
     "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/PostMaloneCandyPaint.jpg/220px-PostMaloneCandyPaint.jpg"
 ];
 
 var links = [
-    "https://www.youtube.com/watch?v=f9gfqGoPGaU",
-    "https://www..com/watch?v=KRaPykuwCiw",
+    "https://www.youtube.com/watch?v=6hd9z-J5FcI",
+    "https://www.youtube.com/watch?v=umHq7Z1ka2Y",
     "https://www.youtube.com/watch?v=0GP7LJxFN84",
     "https://www.youtube.com/watch?v=aJGaZbRL1y0"
 ];
 
 var lengths = [
     "02: 37",
-    "03:11",
+    "03:18",
     "03:41",
     "03:48"
 ];
 
 function displaySongInfo(){
     images.forEach(function(image) {
-        $("#images").append("<img src='" + image + "'>");   
+        $("#images").append("<img src='" + image + "'>");
     });
 
     songs.forEach(function(song) {
-        $("#songs").append("<p>" + song + "</p>"); 
+        $("#songs").append("<p>" + song + "</p>");
     });
 
     artists.forEach(function(artist) {
-        $("#artists").append("<p> By: " + artist + "</p>");   
+        $("#artists").append("<p> By: " + artist + "</p>");
     });
 
     lengths.forEach(function(length) {
-        $("#lengths").append("<p>" + length + "</p>"); 
+        $("#lengths").append("<p>" + length + "</p>");
     });
 
     links.forEach(function(link) {
-        $("#links").append("<a href='" + link + "'> Listen </a>"); 
+        $("#links").append("<a href='" + link + "'> Listen </a>");
     });
 }
 
